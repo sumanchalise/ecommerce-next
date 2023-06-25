c7c19372b4c6d665ddaa5c36fd64072006a26f14
sumanchalise <sumanchalise57@gmail.com>
2023-06-26 00:17:47 +0545

    error page added

---
+++ src/app/error.tsx
@@ -0,0 +1,9 @@
+"use client";
+import { useEffect } from "react";
+
+export default function Error({ error }: { error: Error }) {
+  useEffect(() => {
+    console.error(error);
+  }, [error]);
+  return <div>error</div>;
+}
