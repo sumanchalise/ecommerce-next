75a2fcabc00a9a9b8f7f57f4fc19a0ae32697912
sumanchalise <sumanchalise57@gmail.com>
2023-06-28 17:02:24 +0545

    menuicon added

---
+++ src/icons/MenuIcon.tsx
@@ -0,0 +1,27 @@
+export const MenuIcon = () => {
+  return (
+    <svg viewBox="0 0 24 24" fill="none">
+      <path
+        d="M3 18H21"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M3 12H21"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M3 6H21"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
