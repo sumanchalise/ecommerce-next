d6371390973c56cd30d0432edc407c1f6f2c177c
sumanchalise <sumanchalise57@gmail.com>
2023-06-30 13:12:23 +0545

    Breadcrumb section/component added

--- src/components/BreadCrumb.tsx
+++ src/components/BreadCrumb.tsx
@@ -1,3 +1,17 @@
-export const BreadCrumb = () => {
-  return <div>BreadCrumb</div>;
+export const BreadCrumb = ({
+  title,
+  desc,
+  path,
+}: {
+  title: string;
+  desc: string;
+  path: string;
+}) => {
+  return (
+    <div className="flex flex-col items-center justify-center gap-4 bg-cream py-8">
+      <div className="font-JosefinSans text-5xl font-medium">{title}</div>
+      <div className="max-w-5xl text-center font-Mulish text-lg">{desc}</div>
+      <div className="text-center font-Mulish text-lg">{path}</div>
+    </div>
+  );
 };
