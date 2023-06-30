9b83931d485d8561ce077a97909ac61c3431fdff
sumanchalise <sumanchalise57@gmail.com>
2023-06-30 13:11:45 +0545

    not found/ 404 page added

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,3 +1,24 @@
+import { SadSmileyIcon } from "@/icons/SadSmileyIcon";
+import Link from "next/link";
+
 export default function PageNotFound() {
-  return <div>PageNotFound</div>;
+  return (
+    <div className="flex flex-col items-center justify-center gap-6 py-24 font-Mulish">
+      <div className=" h-10 w-10 ">
+        <SadSmileyIcon />
+      </div>
+      <div className="max-w-sm text-center font-JosefinSans text-4xl font-semibold  capitalize leading-[50px] md:max-w-lg ">
+        the page you are looking for doesn't exist :(
+      </div>
+      <div className="text-base font-medium">
+        We apologize for the inconvinence...
+      </div>
+      <Link
+        href="/"
+        className="pt-5 text-base capitalize text-blue-500 underline hover:text-yellow"
+      >
+        back to home page
+      </Link>
+    </div>
+  );
 }
