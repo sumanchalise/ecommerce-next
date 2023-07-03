f665bae0992f370aba992e0d365d125a68fbd2d6
sumanchalise <sumanchalise57@gmail.com>
2023-07-03 20:33:09 +0545

    minor css changes

--- src/components/BreadCrumb.tsx
+++ src/components/BreadCrumb.tsx
@@ -10,7 +10,7 @@ export const BreadCrumb = ({
   path: string;
 }) => {
   return (
-    <div className="flex flex-col items-center justify-center gap-4 bg-cream py-8">
+    <div className="flex w-full flex-col items-center justify-center gap-4 bg-lightgray py-8">
       <div className="font-JosefinSans text-5xl font-medium">{title}</div>
       <div
         className={clsx("max-w-5xl px-2 text-center font-Mulish text-lg", {
