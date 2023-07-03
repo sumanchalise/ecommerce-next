597c67e3579a993afc6b2e2fa923834362571d32
sumanchalise <sumanchalise57@gmail.com>
2023-07-03 20:55:05 +0545

    WIP

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -14,21 +14,21 @@ export default function page() {
         <div className="flex flex-col items-center justify-center gap-10 px-5">
           <div className="flex w-full flex-col items-center justify-start gap-5 text-middlegray lg:justify-center">
             <div className="flex flex-col items-start  gap-5 lg:flex-row  lg:items-center ">
-              <label className="w-32 text-lg">Email:</label>
+              <label className="w-20 text-lg">Email:</label>
               <input
                 type="email"
                 placeholder="Email"
                 required
-                className=" w-96 rounded-md border-2 border-gray-300 px-3 py-2 leading-tight text-gray-700 focus:outline-none  lg:w-[530px]"
+                className=" w-96 rounded-md border-2 border-gray-300 px-3 py-2 leading-tight text-gray-700 focus:outline-none  lg:w-[490px]"
               />
             </div>
             <div className="flex flex-col items-start  gap-5 lg:flex-row lg:items-center ">
-              <label className="w-32 text-lg">Password:</label>
+              <label className="w-20 text-lg">Password:</label>
               <input
                 type="password"
                 placeholder="Enter Password"
                 required
-                className=" w-96 rounded-md border-2 border-gray-300 px-3 py-2 leading-tight text-gray-700 focus:outline-none  lg:w-[530px]"
+                className=" w-96 rounded-md border-2 border-gray-300 px-3 py-2 leading-tight text-gray-700 focus:outline-none  lg:w-[490px]"
               />
             </div>
           </div>
