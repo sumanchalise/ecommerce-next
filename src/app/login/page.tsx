19f58461e0c905dcea02b8e089f20aad3a682e9c
sumanchalise <sumanchalise57@gmail.com>
2023-07-12 00:46:30 +0545

    login page made responsive

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,48 +1,87 @@
-import { BreadCrumb } from "@/components";
+"use client";
+import { BreadCrumb, Button, Typography } from "@/components";
+import { EyeClosedIcon, EyeOpenIcon } from "@/icons";
+import clsx from "clsx";
 import Link from "next/link";
+import { useState } from "react";
 
-export default function page() {
+export default function Page() {
+  const [isHidden, setIsHidden] = useState(true);
+  const [isDisabled, setIsDisabled] = useState(true);
+
   return (
     <div>
       <BreadCrumb title="Sign-In" desc="" path="Home / Login" />
-      <div className="flex flex-col gap-10 py-14">
-        <div className="flex flex-col items-center justify-center gap-10 px-2 ">
-          <div className="flex flex-col items-center justify-center  font-JosefinSans text-lg uppercase">
-            sign-in your account:
-          </div>
-        </div>
-        <div className="flex flex-col items-center justify-center gap-10 px-5">
-          <div className="text-gray-250 flex w-full flex-col items-center justify-start gap-5 lg:justify-center">
-            <div className="flex flex-col items-start  gap-5 lg:flex-row  lg:items-center ">
-              <label className="w-20 text-lg">Email:</label>
+      <div className="flex flex-col items-center gap-10 px-5 py-14">
+        <Typography varient="heading5" center>
+          SIGN-IN TO YOUR ACCOUNT:
+        </Typography>
+
+        <div className="flex w-full flex-col items-center justify-center gap-10">
+          <div className="flex w-full max-w-sm flex-col items-center gap-5 text-gray-250 lg:max-w-[610px]">
+            <div className="flex w-full flex-col items-start lg:flex-row lg:items-center">
+              <Typography varient="body1" className="w-28">
+                Email:
+              </Typography>
               <input
                 type="email"
-                placeholder="Email"
+                placeholder="Your Email"
                 required
-                className=" w-96 rounded-md border-2 border-gray-300 px-3 py-2 leading-tight text-gray-700 focus:outline-none  lg:w-[490px]"
+                className="w-full rounded-md border-2 border-gray-300 px-3 py-2 leading-tight text-gray-700 focus:outline-none lg:w-[490px]"
               />
             </div>
-            <div className="flex flex-col items-start  gap-5 lg:flex-row lg:items-center ">
-              <label className="w-20 text-lg">Password:</label>
-              <input
-                type="password"
-                placeholder="Enter Password"
-                required
-                className=" w-96 rounded-md border-2 border-gray-300 px-3 py-2 leading-tight text-gray-700 focus:outline-none  lg:w-[490px]"
-              />
+            <div className="flex w-full flex-col items-start lg:flex-row lg:items-center">
+              <Typography varient="body1" className="w-28">
+                Password:
+              </Typography>
+              <div className="flex w-full items-center justify-center rounded-md border-2 border-gray-300 px-3 leading-tight text-gray-700 lg:w-[490px]">
+                <input
+                  type={isHidden ? "password" : "text"}
+                  placeholder="Enter Password"
+                  required
+                  className="w-full py-2 leading-tight text-gray-700 focus:outline-none"
+                />
+                <div
+                  onClick={() => setIsHidden(!isHidden)}
+                  className={clsx("h-4 w-5 cursor-pointer", {
+                    hidden: isHidden,
+                  })}
+                >
+                  <EyeOpenIcon />
+                </div>
+                <div
+                  onClick={() => setIsHidden(!isHidden)}
+                  className={clsx("h-4 w-5 cursor-pointer", {
+                    hidden: !isHidden,
+                  })}
+                >
+                  <EyeClosedIcon />
+                </div>
+              </div>
             </div>
           </div>
-          <div className="flex w-96 flex-col items-center justify-center gap-3">
-            <button className="font-mulish w-full rounded-full bg-yellow py-4 text-base font-medium uppercase tracking-wider">
-              sign in
-            </button>
-            <Link href="#" className="text-lg text-sky underline">
-              Forget your password?
+          <div className="flex w-full flex-col items-center justify-center gap-3">
+            <Button varient="login" disabled={isDisabled}>
+              SIGN IN
+            </Button>
+            <Link href="#">
+              <Typography varient="body1" color="sky" className="underline">
+                Forgot your password?
+              </Typography>
             </Link>
             <div className="flex items-center justify-center gap-2 pt-4 font-JosefinSans">
-              <div className="text-base uppercase">no account?</div>
-              <Link href="/signup" className="text-lg text-sky underline">
-                Create new!
+              <Typography varient="heading5" center>
+                no account?
+              </Typography>
+              <Link href="/signup">
+                <Typography
+                  varient="body1"
+                  color="sky"
+                  center
+                  className="underline"
+                >
+                  Create new!
+                </Typography>
               </Link>
             </div>
           </div>
