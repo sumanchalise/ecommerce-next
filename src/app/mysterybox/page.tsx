8e09ebe1533b24c6e87da3626c58bcb188810aea
sumanchalise <sumanchalise57@gmail.com>
2023-07-12 09:34:53 +0545

    mystery box page added

--- src/app/mysterybox/page.tsx
+++ src/app/mysterybox/page.tsx
@@ -1,3 +1,26 @@
+import {
+  FeaturedComponent,
+  FollowUsFacebook,
+  InterestedOn,
+} from "@/components";
+
 export default function page() {
-  return <div>page</div>;
+  return (
+    <div>
+      <FeaturedComponent />
+      <InterestedOn>you might be interested on</InterestedOn>
+      <FollowUsSection />
+    </div>
+  );
 }
+
+const FollowUsSection = () => {
+  return (
+    <div className="flex flex-col">
+      <div className="flex items-center justify-center py-14 text-center font-Mulish text-3xl md:text-4xl">
+        Follow us on Facebook @SlingsForBabies
+      </div>
+      <FollowUsFacebook />
+    </div>
+  );
+};
