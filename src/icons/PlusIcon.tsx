b711730df841a8fe9de8b8808adb949520ae2107
sumanchalise <sumanchalise57@gmail.com>
2023-07-17 18:24:45 +0545

    icons added

---
+++ src/icons/PlusIcon.tsx
@@ -0,0 +1,17 @@
+export const PlusIcon = () => {
+  return (
+    <svg viewBox="0 0 24 24">
+      <g data-name="Layer 2">
+        <g data-name="plus">
+          <rect
+            width="24"
+            height="24"
+            opacity="0"
+            transform="rotate(180 12 12)"
+          ></rect>
+          <path d="M19 11h-6V5a1 1 0 0 0-2 0v6H5a1 1 0 0 0 0 2h6v6a1 1 0 0 0 2 0v-6h6a1 1 0 0 0 0-2z"></path>
+        </g>
+      </g>
+    </svg>
+  );
+};
---
+++ src/icons/ProhibitionIcon.tsx
@@ -0,0 +1,14 @@
+export const ProhibitionIcon = () => {
+  return (
+    <svg viewBox="0 0 24 24">
+      <path
+        fill="none"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="3"
+        d="M19.141 5A9.97 9.97 0 0 0 12 2C6.477 2 2 6.477 2 12a9.968 9.968 0 0 0 2.859 7M19.14 5A9.967 9.967 0 0 1 22 12c0 5.523-4.477 10-10 10a9.97 9.97 0 0 1-7.141-3M19.14 5L4.86 19"
+      ></path>
+    </svg>
+  );
+};
