0a775fe973fc27d26225a48d87f48223a4470534
sumanchalise <sumanchalise57@gmail.com>
2023-07-18 15:23:13 +0545

    basket page finalized for larger devices

--- src/app/basket/page.tsx
+++ src/app/basket/page.tsx
@@ -48,11 +48,14 @@ export default function page() {
   return (
     <div>
       <BreadCrumb title="Shopping basket" desc="" path="Home / Basket" />
-      <div className="flex flex-col gap-4 px-5 py-7 pb-10 lg:px-10 xl:flex-row 2xl:px-40">
+      <div className="flex flex-col gap-10 px-5 py-7 pb-10 lg:px-10 xl:flex-row xl:gap-2 xl:px-5 2xl:gap-10 2xl:px-40">
         <div className="flex flex-col gap-5">
           <div className="flex w-full flex-col items-center justify-center">
             <div className="hidden w-full items-center justify-center uppercase md:flex">
-              <Typography varient="heading6" className="w-[400px] lg:w-full">
+              <Typography
+                varient="heading6"
+                className="w-[400px] md:w-[600px] lg:w-full "
+              >
                 product
               </Typography>
               <div className="flex w-20 flex-col items-center md:w-24 lg:w-32">
@@ -106,7 +109,7 @@ export default function page() {
                   <div key={basket.id}>
                     <div className="flex flex-col items-start justify-center gap-5 py-5 md:flex-row md:gap-0">
                       <div className="w-full">
-                        <div className="flex w-full gap-2 px-5 md:px-0">
+                        <div className="flex w-full gap-2 md:px-0">
                           <div className="relative h-36 w-32 lg:h-48">
                             <Image
                               src={basket.image}
@@ -213,18 +216,41 @@ export default function page() {
             <Button varient="primary">continue shopping</Button>
           </Link>
         </div>
-        <div>
-          <div className="flex flex-col items-start justify-start border p-4">
-            <div className="uppercase">order summery</div>
-            <div className="div">got a discount code?</div>
-            <div className="flex border">
-              <input type="text" className="" placeholder="Enter your code" />
-              <button>apply</button>
+        <div className="flex h-96 justify-center">
+          <div className="flex flex-col items-start justify-start gap-5 border-2 border-darkgray p-5">
+            <Typography varient="heading6">order summery</Typography>
+            <Typography varient="body1">Got a discount code?</Typography>
+            <div className="flex border-2 border-darkgray p-1">
+              <input
+                type="text"
+                className="px-2 outline-none"
+                placeholder="Enter your code"
+              />
+              <button className="leading-wider flex items-center justify-center gap-1 rounded-full border-2 border-darkblue px-3 py-1">
+                <span className="font-Mulish font-bold uppercase text-darkblue">
+                  apply
+                </span>
+                <div className="h-2 w-3 text-sky">
+                  <TickIcon />
+                </div>
+              </button>
             </div>
-            <div className="flex">sub total:</div>
-            <div className="flex">discount:</div>
-            <div className="flex">grand total:</div>
-            <Button varient="login">proceed to checkout</Button>
+            <div className="flex w-full flex-col gap-2 capitalize">
+              <div className="flex items-center justify-between text-gray-500">
+                <Typography varient="body">sub total:</Typography>
+                <Typography varient="body">$100</Typography>
+              </div>
+              <div className="flex items-center justify-between">
+                <Typography varient="body">discount:</Typography>
+                <Typography varient="body">$1</Typography>
+              </div>
+              <hr className="w-full border-darkgray" />
+              <div className="flex w-full items-center justify-between ">
+                <Typography varient="heading6">grand total:</Typography>
+                <Typography varient="body">$99</Typography>
+              </div>
+            </div>
+            <Button varient="cart">proceed to checkout</Button>
           </div>
         </div>
       </div>
