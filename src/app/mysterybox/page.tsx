74186859d2b34884d379959e20f9a5c51c6da797
sumanchalise <sumanchalise57@gmail.com>
2023-07-27 17:30:07 +0545

    mystery box page added

--- src/app/mysterybox/page.tsx
+++ src/app/mysterybox/page.tsx
@@ -1,33 +1,55 @@
-import {
-  FeaturedComponent,
-  FollowUsFacebook,
-  InterestedOn,
-} from "@/components";
+import { FollowUsFacebook } from "@/components";
+import { ArrowIcon } from "@/icons";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function page() {
   return (
     <div>
       <Mysteryboxsection />
-      <FeaturedComponent />
+      {/* <FeaturedComponent />
       <InterestedOn>you might be interested on</InterestedOn>
-      <FollowUsSection />
+      <FollowUsSection /> */}
     </div>
   );
 }
 
 const Mysteryboxsection = () => {
   return (
-    <div className="flex bg-lightgray px-40 py-7">
-      <div className="w-1/2 bg-red-500">image</div>
-      <div className="w-1/2 bg-blue-500">
-        <div className="div">a</div>
-        <div className="div">v</div>
-        <div className="div">b</div>
-        <div className="div">n</div>
-        <div className="div">m</div>
-        <div className="div">b</div>
-        <div className="div">a</div>
-        <div className="div">h</div>
+    <div className="flex flex-col items-center justify-center gap-5 bg-red-500 py-14 md:flex-row md:px-5 lg:gap-20">
+      <div className="h-[440px] w-full md:max-w-lg xl:h-full">
+        <div className="relative h-[440px] lg:w-[526px] xl:h-[790px]">
+          <Image
+            src="/assets/images/MysteryBox.png"
+            alt="Featured Image"
+            fill
+            sizes="(max-width: 768px) 100vw"
+            className="object-cover"
+            quality={100}
+          />
+        </div>
+      </div>
+      <div className="flex max-w-md flex-col justify-center gap-5 px-10 md:px-0">
+        <h2 className="max-w-lg font-JosefinSans text-4xl font-semibold xl:text-5xl">
+          Parents know parenthood best
+        </h2>
+        <p className="max-w-sm font-Mulish text-lg lg:max-w-md">
+          We&apos;re parents too, and we know how overwhelming it can feel to
+          shop for baby products. That&apos;s why SFB was born—to create a
+          no-brainer shopping experience for the next generation of parents
+          where you can buy quality products at a price that actually makes
+          sense.
+        </p>
+        <Link href="#" className="flex items-center gap-3">
+          <h3 className="font-JosefinSans text-sm font-bold uppercase ">
+            Shop all products
+          </h3>
+          <div className="flex h-10 w-10 cursor-pointer items-center justify-center rounded-full bg-yellow">
+            <div className="h-3 w-5 text-[#0E3C30]">
+              <ArrowIcon />
+            </div>
+          </div>
+        </Link>
       </div>
     </div>
   );
